39f1a5dc5bb2c1693575fe84514a13c5b1b2c9a5
tamtk92 <tamtk92@gmail.com>
2021-08-20 17:01:43 +0700

    Minor changes

--- widgets/multilangfields/MultiLangFieldsWidgetAsset.php
+++ widgets/multilangfields/MultiLangFieldsWidgetAsset.php
@@ -8,7 +8,7 @@ class MultiLangFieldsWidgetAsset extends AssetBundle
 {
     public $sourcePath = '@simsol/multilang/widgets/multilangfields/assets';
     public $css        = [
-        'css/main.css?v=105',
+        'css/main.css?v=106',
     ];
     public $js         = [
     
